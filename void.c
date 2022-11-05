defe6bcc306e0eec222c43fefb14d8cf459632da
bforbiggy <AndyHeCS@protonmail.com>
2022-11-05 15:11:38 -0400

    Formatting changes

--- void.c
+++ void.c
@@ -1,27 +1,27 @@
 #include "void.h"
 
 // Spiral function
-longle r(longle theta){
+longle r(longle theta) {
 	return theta;
 }
 
 // Clears grid
-void clearGrid(){
-	for (int h = 0; h < HEIGHT; h++){
-		for (int w = 0; w < WIDTH; w++){
+void clearGrid() {
+	for (int h = 0; h < HEIGHT; h++) {
+		for (int w = 0; w < WIDTH; w++) {
 			grid[h][w] = ' ';
 		}
 	}
 }
 
 // Updates grid with sampled values
-void updateGrid(){
+void updateGrid() {
 	clearGrid();
 	longle longest = max(WIDTH, HEIGHT);
 
 	longle theta = offset;
 	longle radius = r(theta) - offset;
-	while (radius <= longest){
+	while (radius <= longest) {
 		// Convert to cartesian
 		longle x = radius * cos(theta);
 		longle y = radius * sin(theta);
@@ -29,11 +29,12 @@ void updateGrid(){
 		// Convert to index then modify grid
 		int w = xToW(x, WIDTH);
 		int h = yToH(y, HEIGHT);
-		if(h >= HEIGHT || w >= WIDTH) return;
+		if (h >= HEIGHT || w >= WIDTH)
+			return;
 		grid[h][w] = 'X';
 
 		// Iterate to next sampled point
-		longle multiplier = radius/5.0;
+		longle multiplier = radius / 5.0;
 		multiplier = multiplier > 1 ? multiplier : 1;
 		theta += SAMPLE_RATE / multiplier;
 		radius = r(theta) - offset;
@@ -41,8 +42,8 @@ void updateGrid(){
 }
 
 // Prints grid
-void printGrid(){
-	for (int h = 0; h < HEIGHT; h++){
+void printGrid() {
+	for (int h = 0; h < HEIGHT; h++) {
 		for (int w = 0; w < WIDTH; w++)
 			printf("%c", grid[h][w]);
 		printf("\n");
@@ -50,8 +51,10 @@ void printGrid(){
 	fflush(stdout);
 }
 
-int main(){
-	while(true){
+int main() {
+	while (true) {
+		printf("\x1b[H");
+		fflush(stdout);
 		updateGrid();
 		printGrid();
 
